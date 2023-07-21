659a5d0acd9a98bf6f928bed645958932d01f02a
nickiteks <55086354+nickiteks@users.noreply.github.com>
2023-07-21 10:20:01 +0300

    new dataset model

--- main.py
+++ main.py
@@ -6,52 +6,51 @@ import os
 from skimage import io
 import skimage
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 
-image = io.imread('Logos/bat-logo-preview-400x400.png')
-
-# plotting the original image
-i, (im1, im2, im3, im4) = plt.subplots(1, 4, sharey=True)
-i.set_figwidth(20)
-
-im1.imshow(image)  # Original image
-im2.imshow(image[:, :, 0])  # Red
-im3.imshow(image[:, :, 1])  # Green
-im4.imshow(image[:, :, 2])  # Blue
-i.suptitle('Original & RGB image channels')
+# image = io.imread('Dataset_1/45.jpg')
+#
+# # plotting the original image
+# i, (im1, im2, im3, im4) = plt.subplots(1, 4, sharey=True)
+# i.set_figwidth(20)
+#
+# im1.imshow(image)  # Original image
+# im2.imshow(image[:, :, 0])  # Red
+# im3.imshow(image[:, :, 1])  # Green
+# im4.imshow(image[:, :, 2])  # Blue
+# i.suptitle('Original & RGB image channels')
 
 oldpwd = os.getcwd()
 
-os.chdir('Logos')
+os.chdir('Dataset_1')
 
 files = os.listdir()
 
 data_x = []
 
+#формирование x
 for index in range(100):
     image = io.imread(files[index])
 
     arr = np.array(image)
-
-    data_x.append(np.reshape(arr, (1, 400 * 400 * 4))[0])
+    data_x.append(np.reshape(arr, (1, 400 * 400 * 3))[0])
 
 os.chdir(oldpwd)
 
+# формирование y
 data_y = numpy.random.randint(0, 10, 100)
 data_y = data_y.astype(int)
 print(data_y)
 
-bst = XGBClassifier(n_estimators=2, max_depth=2, learning_rate=1, objective='multi:softprob')
-bst.fit(data_x, data_y)
+X_train, X_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.25)
 
-image = io.imread('Logos/vw-golf-vector-logo-400x400.png')
+bst = XGBClassifier(n_estimators=100, max_depth=2, learning_rate=1, objective='multi:softprob')
+bst.fit(X_train, y_train)
 
-arr = np.array(image)
+y_pred = bst.predict(X_test)
+predictions = [round(value) for value in y_pred]
 
-test = [np.reshape(arr, (1, 400 * 400 * 4))[0]]
-
-print(bst.predict(test))
-
-plt.grid()
-plt.title('Loss by iterations')
-plt.plot(bst.loss_by_iter)
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
