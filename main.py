40356ffdfeee196317ac43275f6426b44f74a056
KDANext <55459738+KDANext@users.noreply.github.com>
2023-07-21 10:31:37 +0300

    Revert "torch mode in file"
    This reverts commit 284809f5f692c32a9042382ef3f6d117f53e8ce2.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import numpy as np
 import numpy.random
+from PIL import Image
+from numpy import asarray
 import os
 from skimage import io
+import skimage
 import matplotlib.pyplot as plt
+from catboost import CatBoostClassifier
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-from models.torch_model import Net
 
 
 count_image = 100
@@ -45,6 +49,30 @@ numpy.random.seed(42)
 data_y = numpy.random.random(count_image)
 print(len(data_x))
 print(len(data_y))
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(4, 6, 5)
+        self.pool = nn.MaxPool2d(3, 3)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(28224, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 0)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 
 net = Net()
 
