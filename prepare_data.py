c1d7582c7adab9a397726bcb7775e6a0671ebeb2
KDANext <55459738+KDANext@users.noreply.github.com>
2023-07-23 15:44:37 +0300

    Create prapare data

---
+++ prepare_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+dataframe = pd.read_csv('data/dataset_2.csv', names=['label', 'style', 'noice', 'place'])
+
+print(dataframe.describe())
+
+dataframe = dataframe.dropna(axis= 0)
+
+print(dataframe.describe())
+
+print(dataframe.size)
+
+market_labels = dataframe.label.unique()
+
+label = dataframe[dataframe.label == market_labels[0]]
+
+print(label['style'].mean())
+
+dataframe_new = pd.DataFrame(columns=dataframe.columns)
+
+for name_img in market_labels:
+    data = [name_img]
+    for column in ['style', 'noice', 'place']:
+        data.append(dataframe[dataframe['label'] == name_img][column].mean())
+    dataframe_new.loc[len(dataframe_new)] = data
+
+print(dataframe_new)
+
