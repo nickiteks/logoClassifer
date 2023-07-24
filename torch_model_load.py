42befb6ca34226c1b0651d5b790d2fa58ae63f74
KDANext <55459738+KDANext@users.noreply.github.com>
2023-07-24 22:25:18 +0300

    remake loader models

--- torch_model_load.py
+++ torch_model_load.py
@@ -1,15 +1,33 @@
 import torch
 from skimage import io
-from model.model import Net
+from model.model3 import Net
 
-net = Net()
-net.load_state_dict(torch.load('model/model.pytorch'))
-net.eval()
+collumns = ['Style', 'Noice', 'Place']
+models = []
 
-arr = io.imread('Dataset_1/16.jpg')
-X = torch.tensor(arr, dtype=torch.float32)
-X = torch.transpose(X,0,2)
+for col in collumns:
+    model = Net()
+    model.load_state_dict(torch.load(f'model/model{col}.pytorch'))
+    model.eval()
+    models.append(model)
 
-print(net(X))
+print('123')
+im = io.imread('Dataset_1/16.jpg')
+im = torch.tensor(im, dtype=torch.float32)
+im.transpose_(0,2)
+im = torch.unsqueeze(im, 0)
+for ml in models:
+    print(ml(im))
+
+#
+# net = Net()
+# net.load_state_dict(torch.load('model/model.pytorch'))
+# net.eval()
+#
+# arr = io.imread('Dataset_1/16.jpg')
+# X = torch.tensor(arr, dtype=torch.float32)
+# X = torch.transpose(X,0,2)
+#
+# print(net(X))
 
 
