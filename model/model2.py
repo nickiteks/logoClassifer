d4411d2a0363b8b21ab7830faf4a426da661460d
KDANext <55459738+KDANext@users.noreply.github.com>
2023-07-24 12:37:17 +0300

    new model simple

---
+++ model/model2.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from tqdm import tqdm
+
+
+class Simple(nn.Module):
+    def __init__(self):
+        super(Simple, self).__init__()
+        self.sq = nn.Sequential(
+            nn.Flatten(1),
+            nn.Linear(480000,3)
+        )
+
+    def forward(self, x):
+        # print(x.shape)
+        x = self.sq(x)
+        return x
